73a23101255bab4b61a68a805595e0aafa2ba87b
felipeboss232 <felipeboss232@gmail.com>
2019-11-02 21:21:49 -0300

    Funçoes / interpretador, interpretando: Var, bool, String, double

--- Fonte/funcoes.java
+++ Fonte/funcoes.java
@@ -2,46 +2,14 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class teste {
-
+	//tem a main pra testar os comandos!!!
 	public static void main(String[] args) {
-		
-		class funcoes{
-			private String atribuicao = ":=";
-			public void interpreta(String programa[]) {
-				for (int linha = 0 ; programa[linha] != null; linha ++) {
-					if (programa[linha].contains(atribuicao)){
-						programa[linha].trim();
-						programa[linha].indexOf(atribuicao);
-						
-						
-						
-					}
-					
-					
-					
-					
-					/*if (programa[linha].contains("==") ||
-						programa[linha].contains("+=") ||
-						programa[linha].contains("-=") ||
-						programa[linha].contains("!=")){
-						
-						
-						
-					}*/	
-				}
-			}
-
-		}
-		
-		
-		
-		
 		//UM JEITO DE PASSAR O ARQUIVO PRO ARGS LINHA POR LINHA
-
-		String a = "a := 10"
+		String a = "testeiriri   := 10"
 				+ "\nb := 15"
-				+ "\nc := a + b"
-				+ "\ns := 'a soma"
+				+ "\nc := testeiriri + b     "
+				+ "\ns := '    a soma'     "
+				+ "\nv :=     True"
 				+ "\nif a+b == c:"
 				+ "\n	if c += 25:"
 				+ "\n		imprime(s + c)"
@@ -56,17 +24,77 @@ public class teste {
 		int i = 0;
         while(sc.hasNextLine()) {
         	programa[i] = sc.nextLine();
-        	System.out.println(programa[i]);
         	i++;
-            //System.out.println(programa[i].getClass().getName());
         }
-        /*System.out.println("".isEmpty());
-        if (programa[10] != null){
-        	System.out.println("".isEmpty());}
-        else {
-        	System.out.println(-1);
-        			
-        	}*/
+        
+		
+		// ESTOU PENSANDO COMO SE TIVESSE 2 OU MAIS OBJEOTS "VARIAVEIS", UM COMO BACKUP
+		// UM PARA STRING E OUTRO PARA FLOAT, POREM TODOS COM OS MESMOS VALORES
+		class Funcoes{
+			private String atribuicao = ":=";
+			
+			public void interpreta(String programa[]) {
+
+				//para todas as linhas que não forem null
+				for (int linha = 0 ; programa[linha] != null; linha ++) {
+
+					//identifica uma atribuição
+					if (programa[linha].contains(this.atribuicao)){
+						
+						programa[linha].trim();
+						int inicioAtribuicao = programa[linha].indexOf(atribuicao);// o indexOf() passa o indice do PRIMEIRO caracter
+						String nomeDaVariavel = programa[linha].substring(0, inicioAtribuicao).replace(" ", ""); //nome da variavel sem NENHUM ESPAÇO
+						Double valor = 0.0;
+						
+						//tenta passar para double
+						try{
+				        	valor = Double.parseDouble(programa[linha].split("=")[1]);
+				        	System.out.print(nomeDaVariavel + "|");
+				        	System.out.println(valor);
+				        	//System.out.println(programa[linha].split("'")[1]);
+				        } 
+						//resolve o erro se nao for double
+						catch (NumberFormatException e) {
+				        	//Se tiver um + é atribuicao TRATAR MELHOR ISSO
+							if (programa[linha].split("=")[1].contains("+")) {
+				        		System.out.print(nomeDaVariavel + "|");
+				        		System.out.println(programa[linha].split("=")[1].trim());
+				        	}
+							//se tiver ' é string JA RESOLVIDO
+							else if (programa[linha].split("=")[1].contains("'")) {
+				        		System.out.print(nomeDaVariavel + "|");
+				        		System.out.println(programa[linha].split("=")[1].trim());
+				        	}
+							//Verifica se é booleano, TRATAR
+							else if (programa[linha].split("=")[1].toLowerCase().contains("true") || 
+				        			  programa[linha].split("=")[1].toLowerCase().contains("false")) {
+				        		System.out.print(programa[linha].substring(0, programa[linha].indexOf(":=") ).replace(" ", "") + "|");
+				        		System.out.println("bool");
+				        	}
+
+				        	//Double valor = Double.parseDouble(programa[2].split("=")[1]);
+				        	//System.out.println(valor);
+				        }
+						
+
+						
+						
+						
+					}
+				}
+			}
+
+		}
+		
+		//####################
+		//#TESTE DO PROGRAMA #
+		//####################
+		
+		
+		Funcoes teste = new Funcoes(); 
+		teste.interpreta(programa);
+
+
         }
         
 	}
