4bf965e2343ca558e455a11a260e0aa6c1f55046
felipeboss232 <felipeboss232@gmail.com>
2019-11-09 02:08:21 -0300

    Ajeitado Codigo e preparando operaçoes

--- Fonte/Funcoes.java
+++ Fonte/Funcoes.java
@@ -7,17 +7,16 @@ public class Funcoes extends ClasseVariaveis{
 	private String[] operadoresMatematicos = {"+", "-", "*", "/", "%"};
 	private String[] operadoresBooleanos = {"==", "!=", "@or", "@and", ">", "<"}; //não sei oq fazer com a or e a and
 	
-	static private ClasseVariaveis interpretador = new ClasseVariaveis();
+	//talvez um nome melhor para a variavel fosse "m" !?
+	static private ClasseVariaveis memoria = new ClasseVariaveis();
 			
 			
 	public void interpreta(String programa[]) {
-
 		//para todas as linhas que não forem null
 		for (int linha = 0 ; programa[linha] != null; linha ++) {
 
 		//identifica uma atribuição
-			if (programa[linha].contains(this.atribuicao)){
-						
+			if (programa[linha].contains(this.atribuicao)){		
 				programa[linha].trim();
 				int inicioAtribuicao = programa[linha].indexOf(atribuicao);// o indexOf() passa o indice do PRIMEIRO caracter
 				String nomeDaVariavel = programa[linha].substring(0, inicioAtribuicao).replace(" ", ""); //nome da variavel sem NENHUM ESPAÇO
@@ -28,12 +27,10 @@ public class Funcoes extends ClasseVariaveis{
 				try{							
 		        	valor = Double.parseDouble(programa[linha].split("=")[1].replace(" ", ""));
 		        	//GUARDA COMO STRING
-		        	if (interpretador.indiceVariavel(nomeDaVariavel) == -1){
-		        		interpretador.setVariavel(nomeDaVariavel, valor.toString());
-		        	}else {interpretador.setConteudo(nomeDaVariavel, valor.toString());
+		        	if (memoria.indiceVariavel(nomeDaVariavel) == -1){
+		        		memoria.setVariavel(nomeDaVariavel, valor.toString());
+		        	}else {memoria.setConteudo(nomeDaVariavel, valor.toString());
 		        		}
-		        	interpretador.imprimeVariavel(interpretador.indiceVariavel(nomeDaVariavel));
-	        		//System.out.println(interpretador.getConteudo(nomeDaVariavel).getClass());
 		        }
 
 				//resolve o erro se nao for double
@@ -41,36 +38,49 @@ public class Funcoes extends ClasseVariaveis{
 					//Reconhece qualquer string dentro dos operadoresBooleanos, descobri o "for each" :)
 					for(String k : operadoresBooleanos) {
 						if (programa[linha].split("=")[1].contains(k)) {
-			        		String Booleano = "Booleano";
-			        		interpretador.setVariavel(nomeDaVariavel, Booleano);
-			        		interpretador.imprimeVariavel(interpretador.indiceVariavel(nomeDaVariavel));
+							String Booleano = "Booleano";
+			        		if(memoria.indiceVariavel(nomeDaVariavel) == -1) {
+			        			memoria.setVariavel(nomeDaVariavel, Booleano);
+			        		}else {
+			        			memoria.setConteudo(nomeDaVariavel, "outro booleano");
+			        		}
 						}
 					}
 
 					//Reconhece qualquer string dentro dos operadoresMatematicos, descobri o "for each" :)
-					//TRATAR
 					for(String l : operadoresMatematicos){
 						if (programa[linha].split("=")[1].contains(l)) {
-							interpretador.setVariavel(nomeDaVariavel, "Equação a resolver, STRING");
-			        		interpretador.imprimeVariavel(interpretador.indiceVariavel(nomeDaVariavel));
+							if(memoria.indiceVariavel(nomeDaVariavel) == -1) {
+								memoria.setVariavel(nomeDaVariavel, programa[linha].split("=")[1].trim().replace(" ", ""));
+							}else {
+								memoria.setConteudo(nomeDaVariavel, programa[linha].split("=")[1].trim().replace(" ", ""));
+							}
 						}
 					}
 
 					//se tiver ' é string
 					if (programa[linha].split("=")[1].contains("'")) {
-		        		if(interpretador.indiceVariavel(nomeDaVariavel) == -1){
-		        			interpretador.setVariavel(nomeDaVariavel, programa[linha].split("=")[1].trim().replace("'", ""));
+		        		if(memoria.indiceVariavel(nomeDaVariavel) == -1){
+		        			memoria.setVariavel(nomeDaVariavel, programa[linha].split("=")[1].trim().replace("'", ""));
 		        		}else {
-			        		interpretador.setVariavel(nomeDaVariavel, programa[linha].split("=")[1].trim().replace("'", ""));
+			        		memoria.setVariavel(nomeDaVariavel, programa[linha].split("=")[1].trim().replace("'", ""));
 		        		}
 		        	}
 
 					//Verifica se é booleano, resolver com o IF
 					else if (programa[linha].split("=")[1].toLowerCase().contains("true")) {
-						interpretador.setVariavel(nomeDaVariavel, "true");
+						if(memoria.indiceVariavel(nomeDaVariavel) == -1) {
+							memoria.setVariavel(nomeDaVariavel, "true");
+						}else {
+							memoria.setConteudo(nomeDaVariavel, "true");
+						}
 		        	}
 					else if (programa[linha].split("=")[1].toLowerCase().contains("false")) {
-						interpretador.setVariavel(nomeDaVariavel, "false");
+						if(memoria.indiceVariavel(nomeDaVariavel) == -1) {
+							memoria.setVariavel(nomeDaVariavel, "false");
+						}else {
+							memoria.setConteudo(nomeDaVariavel, "false");
+						}
 					}
 					
 					//reconhece se é um numero com ;
@@ -78,33 +88,41 @@ public class Funcoes extends ClasseVariaveis{
 		        		if (programa[linha].split("=")[1].contains(";")) {
 		        			System.out.print(nomeDaVariavel + "|");
 		    				System.out.println(Double.parseDouble(programa[linha].split("=")[1].replace(" ", "").replace(";", "")));
-		    			}
-				        		
+		    			}	
 		        	}
-
-							
-							
 		        }//fim do catch
-						
-
-						
-						
-						
 			}//fim do if "atribuição"
 		}//fim do for para cada linha diferente de null
+		
+		 /*
+		int contador = 0;
+		System.out.println("________________________________________________________________________");
+		for(String x : conteudos) {
+			if(x != null) {
+				System.out.println(variaveis[contador] + " | " + x);
+				contador ++;
+			}
+		}
+		
+		System.out.println(conteudos[1]);
+		*/
 	}//fim do metodo interpreta
+	
 
+	public String operacoesMat(String expressao){
+		
+		return "x";
+	}
 		
 		//####################
 		//#TESTE DO PROGRAMA #
 		//####################
 		//tem a main pra testar os comandos
 public static void main(String[] args) {
-	//UM JEITO DE PASSAR O ARQUIVO PRO ARGS LINHA POR LINHA
 	String a = "a   := 10"
 				+ "\nb := 15"
 				+ "\nc := a + b     "
-				+ "\nc := a> b     "
+				+ "\nd := a> b     "
 				+ "\ns := '  ;  a soma'     "
 				+ "\n//um comentario"
 				+ "\nv :=     True"
@@ -114,7 +132,6 @@ public static void main(String[] args) {
 				+ "\n	else:"
 				+ "\n		while 25> c:"
 				+ "\n			c++";
-	//System.out.println(a);
 		
 	String programa[] = new String[1000];
 
@@ -124,11 +141,10 @@ public static void main(String[] args) {
        	programa[i] = sc.nextLine();
        	i++;
     }		
-	
-    
 	Funcoes teste = new Funcoes(); 
 	teste.interpreta(programa);
-	System.out.println(ClasseVariaveis.getConteudo(3));
 	sc.close();
+	
+	
 	}//fim da main
 }//fim da classe funçoes
